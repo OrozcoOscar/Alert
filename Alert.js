ebde05231d7fd97f77c439ea412b98fd891552e0
Orozco Oscar <88407386+OrozcoOscar@users.noreply.github.com>
2022-09-19 12:25:10 -0500

    Add files via upload
    Versión 1

---
+++ Alert.js
@@ -0,0 +1,90 @@
+/**
+ * OrozcoOscar
+ * v1.0
+ * 19/09/22
+ */
+/**
+     * Genera una Alerta con Alert.alert(...)
+     */
+class Alert {
+    constructor(){
+        this.opction
+    }
+    /**
+     * Genera una Alerta
+     * @param text { title:String|HTML, contenido:String|HTML }.
+     * @param opction Configuracion de botones Ejem. [
+     * { name: "Aceptar", -> Nombre del Boton
+     *  onclick:()=>{}, -> Accion a Realizar despues del Click
+     *  type: 1 -> tipo de Boton [1,2,3,4,5]
+     *  }]
+     */
+    static alert(text = { title: "", contenido: "" },
+     opction = [{ name: "Aceptar", onclick:()=>{}, type: 1 }]) {
+        this.opction=opction
+        let css = `
+            width: -webkit-fill-available;
+            height: -webkit-fill-available;
+            background: #000000e3;
+            position: absolute;
+            top: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        `
+        let css2 = `
+            width: 50%;
+            min-height: 4rem;
+            background: white;
+            padding: 1rem;
+            border-radius: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            `
+        let cssColors = ["#2ba52b;", "#a83828;", "#696868;", "#39659d;", "#ede814;"]
+
+        let div = document.createElement("div")
+        div.id="ALERTMODAL"
+        div.setAttribute("style", css)
+        let aux = ""
+        opction.map(op =>{
+            let temp=`<button 
+
+            onclick="Alert.close('${op.name}')"
+            style="padding: .5rem;
+            border: none;
+            background: ${cssColors[op.type - 1]}
+            color: white;
+            border-radius: 5px;
+            margin: 1rem;
+            cursor:pointer;
+        "
+        >${op.name}</button>`
+        aux +=temp
+    })
+
+        div.innerHTML = `<div  style="${css2}">
+                        <div>
+                            <h1>
+                                ${text?.title||""}
+                            </h1>
+                            <p>${text?.contenido||""}</p>
+                        </div>
+                        <div>
+                            ${aux}
+                        </div>
+                    </div>`
+        document.body.append(div)
+
+
+    }
+    static close(e) {
+       try{
+        this.opction.filter(op=>op.name==e)[0].onclick()
+       }catch(e){
+        console.error("onclick is undefined")
+       }
+        document.querySelector("#ALERTMODAL").remove()
+    }
+}
